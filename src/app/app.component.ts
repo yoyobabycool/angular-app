83fad4b38e28f5a809592b975b203f91ec1225a1
yoyobabycool <nspt.saiphaniteja@gmail.com>
2023-06-24 23:23:33 +0530

    added basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
+    poppedPopcorns: any[] = [];
+  addPopcorn(event: MouseEvent) {
+      const popcorn = {
+      x: event.clientX - 25,
+      y: event.clientY - 25,
+      falling: false
+    };
+    this.poppedPopcorns.push(popcorn);
+  this.playPopcornSound()
+    setTimeout(() => {
+      popcorn.falling = true;
+      setTimeout(() => {
+        this.poppedPopcorns = this.poppedPopcorns.filter(p => p !== popcorn);
+      }, 2000);
+    }, 100);
+  }
+    playPopcornSound() {
+    const audio = new Audio("assets/popcorn_sound.mp3");
+    audio.play();
+  }
 }
