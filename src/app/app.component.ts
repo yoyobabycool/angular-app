c7e3dbd00d86db04efd3ab9180eab129fe76fddd
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2024-06-11 20:11:38 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -61,7 +61,7 @@ export class AppComponent implements OnInit {
   const targetUrl = 'https://api-partner.spotify.com/pathfinder/v1/query?operationName=getTrack&variables=%7B%22uri%22%3A%22spotify%3Atrack%3A6b2WJDzGt5X8dYfpkWtvXW%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22ae85b52abb74d20a4c331d4143d4772c95f34757bfa8c625474b912b9055b5c0%22%7D%7D';
   const headers = {"Authorization" : "Bearer BQCylPN6TvhRTLmsj1j-x_dhcvN99Xt6sR5Fk6-qBtSnJX63jMYTf1pJdF4ASD6aU0B1pBlgVt48MG_Za8hoBfOPEBP52JtcK0Pm5Me2X8Pse748s58"}
   const url = targetUrl;
-    this.http.get(url)
+    this.http.get(url, {headers})
       .subscribe(response => {
         this.data = response;
         console.log(this.data);
