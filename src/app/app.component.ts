a152e238d224adf28e2df239d2837ee25e4e9a8f
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2024-06-13 21:32:14 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,7 +59,8 @@ export class AppComponent implements OnInit {
   }
   getDataspot(): void {
   const targetUrl = 'https://api-partner.spotify.com/pathfinder/v1/query?operationName=getTrack&variables=%7B%22uri%22%3A%22spotify%3Atrack%3A6b2WJDzGt5X8dYfpkWtvXW%22%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%22ae85b52abb74d20a4c331d4143d4772c95f34757bfa8c625474b912b9055b5c0%22%7D%7D';
-  const headers = {"Authorization" : "'Bearer'+this.token.accessToken"}
+  const tok = 'Bearer ' + this.token.accessToken
+  const headers = {"Authorization" : tok}
   const url = targetUrl;
     this.http.get(url, {headers})
       .subscribe(response => {
