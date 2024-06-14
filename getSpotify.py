30d71f4def54d2897fd6494ad9f7289cdd4102a7
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2024-06-14 16:53:14 +0530

    Update getSpotify.py

--- getSpotify.py
+++ getSpotify.py
@@ -7,16 +7,14 @@ head = "Bearer "+ access_token
 url = "https://api-partner.spotify.com/pathfinder/v1/query?operationName=fetchPlaylist&variables=%7B%22uri%22%3A%22spotify%3Aplaylist%3A15uBOFXJMjU3u5BEf3frVO%22%2C%22offset%22%3A0%2C%22limit%22%3A25%7D&extensions=%7B%22persistedQuery%22%3A%7B%22version%22%3A1%2C%22sha256Hash%22%3A%2291d4c2bc3e0cd1bc672281c4f1f59f43ff55ba726ca04a45810d99bd091f3f0e%22%7D%7D"
 headers={"Authorization" : head}
 response2 = requests.get(url, headers=headers)
-data = response.json()
+data = response2.json()
 tracks = []
 
 for item in data["data"]["playlistV2"]["content"]["items"]:
     track_data = item["itemV2"]["data"]
-    img = ["albumOfTrack"]["coverArt"]
     tracks.append({
         "name": track_data["name"],
-        "playcount": track_data["playcount"],
-        "image": img.sources[0].url
+        "playcount": track_data["playcount"]
     })
 
 result = {
