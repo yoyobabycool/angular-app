e0aacfdb1ecfda6ec1aa9c2a2a5cbcd4f915a992
yoyobabycool <121718122+yoyobabycool@users.noreply.github.com>
2024-09-10 22:21:33 +0530

    modify url

--- src/app/video-data.service.ts
+++ src/app/video-data.service.ts
@@ -7,7 +7,7 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class VideoDataService {
-  private apiUrl = 'https://afraid-clouds-smile.loca.lt/getstats';  // API URL
+  private apiUrl = 'https://pink-maps-study.loca.lt/getstats';  // API URL
 
   constructor(private http: HttpClient) { }
 
